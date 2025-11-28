c7b1ecb36fa4c76da59fe0ae11d128fdeb997715
samian04 <samiansikdar04@gmail.com>
2025-11-28 13:06:09 +0600

    uploading in github

---
+++ second/src/server.js
@@ -0,0 +1,17 @@
+import express from "express";
+import path, { dirname } from "path";
+import { fileURLToPath } from "url";
+const app = express();
+const PORT = process.env.PORT || 8080;
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename)
+ app.use(express.static(path.join(__dirname , "../public")))
+app.get(`/`, (req, res) => {
+    res.sendFile(path.join(__dirname ,"../public","index.html"))
+
+})
+
+app.listen(PORT, () => {
+     console.log(`OUR FUCKING SERVER IS RUNNING ON PORT:${PORT}`);
+     
+ })
